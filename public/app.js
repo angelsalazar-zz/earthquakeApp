555bb244033a62380dd0817b5c9bd9e5f2a5fb1d
Angel Salazar Marcatoma <Angel Salazar Marcatoma>
2016-12-23 19:06:54 -0600

    google maps api for cities added

--- public/app.js
+++ public/app.js
@@ -20,18 +20,21 @@ function onWindowLoad () {
   };
 
   // Get widgets control
-  var searchForm   = document.querySelector('#searchForm');
-  var inputWrapper = document.querySelector('#inputWrapper');
-  var place        = document.querySelector('#place');
-  var sortByDate   = document.querySelector('#time');
-  var displayName  = document.querySelector('#display');
-  var currentDate  = document.querySelector('#currentDate');
-  var currentDate  = document.querySelector('#currentDate');
-  var hint         = document.querySelector('#hint');
-  var restore      = document.querySelector('#restore');
-  var toggleMap    = document.querySelector('#toggleMap');
-  var map          = document.querySelector('#map');
-  var topTenTable  = document.querySelector('#topTenTable');
+  var searchForm    = document.querySelector('#searchForm');
+  var inputWrapper  = document.querySelector('#inputWrapper');
+  var place         = document.querySelector('#place');
+  var sortByDate    = document.querySelector('#time');
+  var displayName   = document.querySelector('#display');
+  var currentDate   = document.querySelector('#currentDate');
+  var currentDate   = document.querySelector('#currentDate');
+  var hint          = document.querySelector('#hint');
+  var restore       = document.querySelector('#restore');
+  var toggleMap     = document.querySelector('#toggleMap');
+  var map           = document.querySelector('#map');
+  var topTenTable   = document.querySelector('#topTenTable');
+  var toggleMapCity = document.querySelector('#toggleMapCity');
+  var mapForCity    = document.querySelector('#mapForCity');
+  var earthquakeTable = document.querySelector('#earthquakeTable');
 
   // Setting current date
   currentDate.innerHTML = topTenEarthquakes.date;
@@ -44,7 +47,8 @@ function onWindowLoad () {
   var dataManager       = new DataManager();
   var ajax              = new Ajax();
   var googleMap;
-
+  var googleMapForCity;
+  var cityCenter;
   // Define map options
   var mapOptions   = {
     center: {
@@ -54,6 +58,10 @@ function onWindowLoad () {
     scrollwheel: false,
     zoom: 1
   };
+  var mapForCityOptions = {
+    scrollwheel: false,
+    zoom: 4
+  }
   ajax
     .request({
       type : "GET",
@@ -75,7 +83,7 @@ function onWindowLoad () {
   sortByDate.addEventListener('click', onSort);
   restore.addEventListener('click', onRestore);
   toggleMap.addEventListener('click', onToggleMap);
-
+  toggleMapCity.addEventListener('click', onToggleMapForCity);
   // CSS classes
   var successClass = 'has-success';
   var errorClass   = 'has-error';
@@ -105,6 +113,7 @@ function onWindowLoad () {
               west  : response.results[0].geometry.bounds.southwest.lng,
               username : geoNamesUsername
             }
+            cityCenter = response.results[0].geometry.location;
             displayName.innerHTML = response.results[0].formatted_address;
             return ajax.request({
               type : "GET",
@@ -125,8 +134,14 @@ function onWindowLoad () {
           inputWrapper.classList.remove(successClass)
           place.blur();
           spinLoader('searchLoader', ajax.isLoading());
-          if (restore.classList.contains('hide'))
+          if (restore.classList.contains('hide') && toggleMapCity.classList.contains('hide')) {
             restore.classList.toggle('hide');
+            toggleMapCity.classList.toggle('hide');
+          }
+          if (!mapForCity.classList.contains('hide')) {
+            mapForCityOptions.center = cityCenter;
+            initMap(googleMapForCity, mapForCity, mapForCityOptions, dataManager);
+          }
         })
         .catch(function (error) {
           alert(error);
@@ -174,10 +189,19 @@ function onWindowLoad () {
   // Click callback for toggleMap button
   function onToggleMap (e) {
     topTenTable.classList.toggle('hide');
-    var hide = map.classList.toggle('hide');
-    if (!hide) {
+    var hidden = map.classList.toggle('hide');
+    if (!hidden) {
       googleMap = new google.maps.Map(map, mapOptions);
       setMarkers(googleMap, topTenDataManager.getData());
+    }
+  }
+  // Click callback for toggleMapForCity button
+  function onToggleMapForCity(e) {
+    earthquakeTable.classList.toggle('hide');
+    var hidden = mapForCity.classList.toggle('hide');
+    if (!hidden) {
+      mapForCityOptions.center = cityCenter;
+      initMap(googleMapForCity, mapForCity, mapForCityOptions, dataManager);
     }
   }
   // Helpers
@@ -246,5 +270,8 @@ function onWindowLoad () {
               });
       })
   }
-
+  function initMap(googleMapObject, mapWidget, mapOptionsObject, dataManagerObject) {
+    googleMapObject = new google.maps.Map(mapWidget, mapOptionsObject);
+    setMarkers(googleMapObject, dataManagerObject.getData());
+  }
 };
